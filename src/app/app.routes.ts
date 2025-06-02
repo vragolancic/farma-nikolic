7105df47a165d609da1eeab7bc2c2581b417b41b
Ivica Nikolic <ivicanikolic.mail@gmail.com>
2025-06-02 17:05:19 +0200

    Podesavanje admin templatea

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {Admin} from './admin/admin';
+import {TestAdmin} from './admin/test-admin/test-admin';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "admin",
+    component: Admin,
+    children: [
+      {path:"test", component:TestAdmin}
+    ]
+  }
+];
